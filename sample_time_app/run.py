5e6e5a9f5e3f5446a19d02240ecd94a92f7f001f
Talia193 <149318247+Yutong41319@users.noreply.github.com>
2026-09-24 00:34:32 -0400

    Update sample time app

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,12 +1,18 @@
 from flask import Flask
-app = Flask(__name__)
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
 
+@app.route('/time')
+def current_time():
+    return datetime.now(ZoneInfo("America/New_York")).strftime('%Y-%m-%d %H:%M:%S')
 
 app.run(host='0.0.0.0',
         port=8080,
         debug=True)
+
